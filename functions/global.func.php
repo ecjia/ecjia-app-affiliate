9c4e6da680136b8bf5cbc3269b6e811e403786f7
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-29 10:52:26 +0800

    整理商品下的废弃方法

--- functions/global.func.php
+++ functions/global.func.php
@@ -1,6 +1,9 @@
 <?php
 defined('IN_ECJIA') or exit('No permission resources.');
 
+/**
+ * 添加管理员记录日志操作对象
+ */
 function assign_adminlog_content() {
 	ecjia_admin_log::instance()->add_object('affiliate', RC_Lang::get('affiliate::affiliate.affiliate'));
 	ecjia_admin_log::instance()->add_object('config', RC_Lang::get('affiliate::affiliate.config'));
