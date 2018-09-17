917939573872acb7bbd450e2210fc3e55d27b6db
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-09-17 10:13:15 +0800

    affiliate推荐模块接口修改

--- modules/invite/record_module.class.php
+++ modules/invite/record_module.class.php
@@ -50,7 +50,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * 推荐奖励的记录
  * @author will.chen
  */
-class record_module extends api_front implements api_interface {
+class invite_record_module extends api_front implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {	
 		$this->authSession();
 		if ($_SESSION['user_id'] <= 0 ) {
