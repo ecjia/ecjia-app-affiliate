982ea69c08281d18359f3b0c1b9c4b42c0b207cc
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-09-25 11:07:06 +0800

    订单管理修改为订单分成

--- apis/affiliate_admin_menu_api.class.php
+++ apis/affiliate_admin_menu_api.class.php
@@ -57,7 +57,7 @@ class affiliate_admin_menu_api extends Component_Event_Api {
 		
 		$submenus = array(
 			ecjia_admin::make_admin_menu('affiliate', __('分成比例', 'affiliate'), RC_Uri::url('affiliate/admin/init'), 1)->add_purview('affiliate_percent_manage'),
-			ecjia_admin::make_admin_menu('affiliate_ck', __('分成管理', 'affiliate'), RC_Uri::url('affiliate/admin_separate/init'), 2)->add_purview('affiliate_ck_manage')
+			ecjia_admin::make_admin_menu('affiliate_ck', __('分成订单', 'affiliate'), RC_Uri::url('affiliate/admin_separate/init'), 2)->add_purview('affiliate_ck_manage')
 		);
 		$menus->add_submenu($submenus);
 		return $menus;
